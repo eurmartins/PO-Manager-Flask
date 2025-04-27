7501281e8b72f6c2a22132aa1814f3a0d767ec80
Eurmartins <victormartinssantos.work@gmail.com>
2025-04-27 06:12:05 -0300

    refactor: refactoring the user service along with the user repository

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,28 +1,30 @@
+from typing import Any, Dict, Tuple, Optional
+from marshmallow import ValidationError
+
 from app.repositories.user_repository import UserRepository
 from app.schemas.user_schemas import UserCreateSchema
 from app.models.user import User
-from marshmallow import ValidationError
 
 class UserService:
     def __init__(self):
         self.repository = UserRepository()
+        self.create_schema = UserCreateSchema()
 
-    def get_user_by_id(self, user_id):
+    def get_user_by_id(self, user_id: int) -> Optional[User]:
         return self.repository.get_by_id(user_id)
 
-    def get_user_by_username(self, username):
+    def get_user_by_username(self, username: str) -> Optional[User]:
         return self.repository.get_by_username(username)
 
-    def get_user_by_email(self, email):
+    def get_user_by_email(self, email: str) -> Optional[User]:
         return self.repository.get_by_email(email)
 
-    def get_all_users(self):
+    def get_all_users(self) -> list[User]:
         return self.repository.get_all()
 
-    def create_user(self, data):
-        schema = UserCreateSchema()
+    def create_user(self, data: dict) -> Tuple[Dict[str, Any], int]:
         try:
-            validated_data = schema.load(data)
+            validated_data = self.create_schema.load(data)
         except ValidationError as err:
             return {"errors": err.messages}, 400
 
@@ -33,18 +35,19 @@ class UserService:
             return {"message": "Email já em uso"}, 400
 
         user = User(
-            id=None,
             username=validated_data["username"],
             password=validated_data["password"],
             email=validated_data["email"],
             role=validated_data["role"]
         )
 
-        return self.repository.create(user), 201
+        created_user = self.repository.create(user)
+        return {"message": "Usuário criado com sucesso", "user_id": created_user.id}, 201
 
-    def delete_user(self, user_id):
+    def delete_user(self, user_id: int) -> Tuple[Dict[str, str], int]:
         user = self.repository.get_by_id(user_id)
         if not user:
             return {"message": "Usuário não encontrado"}, 404
+
         self.repository.delete(user)
         return {"message": "Usuário deletado com sucesso"}, 200
